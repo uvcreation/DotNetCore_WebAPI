d0834c21c64f92ec320c3afd3ae9847c50b4f886
Uttam Vyas <uttamvs031@gmail.com>
2020-12-30 01:16:44 +0530

    Docker file created and modified the Kestrel port setting

--- Client/WebAPI/Program.cs
+++ Client/WebAPI/Program.cs
@@ -1,6 +1,9 @@
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Serilog;
+using System;
+using System.IO;
 
 namespace WebAPI
 {
@@ -8,15 +11,24 @@ namespace WebAPI
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .UseSerilog()
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false)
+                .Build();
+            
+            var port = Environment.GetEnvironmentVariable("PORT");
+
+            var webHost = WebHost.CreateDefaultBuilder(args)
+                .UseUrls("http://*:" + port)
+                .UseKestrel()
+                .UseStartup<Startup>();
+
+            return webHost;
+        }
     }
 }
